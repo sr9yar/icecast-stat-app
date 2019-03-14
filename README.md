cc052b463f90fd891191f07fbc40fc451e26a6f4
sr9yar <340999@gmail.com>
2019-03-14 22:00:44 +0300

    added three test methods to return data from icecast server

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # icecast-stat-app
 
+## Prerequisites
+
+
+Create a `.env` file in the project root. 
+An example can be found in `.env.example`.
+
+
+There three routes at the momet:
+
+
+GET /getServerInfo
+
+GET /getListeners
+
+GET /listclients
+
+
+Check console for more detail.
+
+
 ## To start the app run 'npm start'
+
+## To stop the server press 'ctrl+C'
+
--- index.js
+++ index.js
@@ -3,25 +3,31 @@
 const Hapi=require('hapi');
 const Monitor = require('icecast-monitor');
 
+require('dotenv').config();
+
+
 const monitor = new Monitor({
-  host: 'icecast.dev',
-  port: 80,
-  user: 'admin',
-  password: 'hackme'
+  host: process.env.ICECAST_HOST,
+  port: process.env.ICECAST_PORT,
+  user: process.env.ICECAST_USER,
+  password: process.env.ICECAST_PASSWORD
 });
 
 
 const server=Hapi.server({
-    host:'localhost',
-    port:8000
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || '8000'
 });
 
 // Main route
 server.route({
     method:'GET',
     path:'/',
-    handler:function(request,h) {
-        return 'main';
+    handler: (request,h) => {
+
+
+
+        return `PORT ${process.env.PORT}`;
     }
 });
 
@@ -30,11 +36,110 @@ server.route({
 server.route({
     method:'GET',
     path:'/hello',
-    handler:function(request,h) {
+    handler:(request,h) => {
 
         return 'hello';
     }
 });
+
+
+
+
+
+// getServerInfo  route
+server.route({
+    method:'GET',
+    path:'/getServerInfo',
+    handler: async (request,h) => {
+
+        return await new Promise((resolve, reject) =>{
+            monitor.getServerInfo(function(err, server) {
+                if (err) reject( err );
+                resolve( server );
+            });
+        });
+
+    }
+});
+
+
+
+// getListeners  route
+server.route({
+    method:'GET',
+    path:'/getListeners',
+    handler: async (request,h) => {
+
+        return await new Promise((resolve, reject) =>{
+            monitor.getListeners(function(err, listeners) {
+                if (err) reject( err );
+                resolve( listeners );
+              });
+
+        });
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+// listclients  route
+server.route({
+    method:'GET',
+    path:'/listclients',
+    handler: async (request,h) => {
+
+        const url = '/admin/listclients';
+        // const url = '/admin/stats';
+        
+        return await new Promise((resolve, reject) =>{
+            // Collect sources without storing them in a memory
+            monitor.createStatsXmlStream( url,  (err, xmlStream) => {
+                if (err) reject( err );
+                
+                const xmlParser = new Monitor.XmlStreamParser();
+                
+                const arr = []
+                xmlParser.on('error', (err) => {
+                    console.log('error', err); 
+                    reject( err );
+                });
+                
+                xmlParser.on('source', (source) => {
+                    // Do work with received source
+                    console.log('source', source);
+                    arr.push( source );
+                });
+            
+                // Finish event is being piped from xmlStream
+                xmlParser.on('finish', () => {
+                    console.log('all sources are processed');
+                    resolve( arr );
+                });
+            
+                xmlStream.pipe( xmlParser );
+            });
+
+        });
+
+
+    }
+});
+
+
+
+
+
+
+
+
 
 
 // Start the server
